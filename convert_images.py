666243f6df27cda8e0a23952ace5628dc942926f
Tim Felgentreff <timfelgentreff@gmail.com>
2021-02-28 13:58:52 +0100

    import warcraft project into new toolchain setup

---
+++ convert_images.py
@@ -0,0 +1,125 @@
+import glob
+import math
+import os
+import re
+import subprocess
+
+TILE_SIZE = 16
+
+DATA = {
+    # "CURSORS": [
+    #     "graphics/ui/cursors/upper_left_arrow.png",
+    #     "graphics/ui/cursors/magnifying_glass.png",
+    #     "graphics/ui/cursors/red_crosshair.png",
+    # ],
+    "HUMAN_PANEL": [
+        "graphics/tilesets/forest/portrait_icons.png",
+        "graphics/ui/gold_icon_1.png",
+        "graphics/ui/lumber_icon_1.png",
+        "graphics/ui/percent_complete.png",
+        "graphics/ui/human/icon_border.png",
+        "graphics/ui/human/icon_selection_boxes.png",
+        "graphics/ui/human/panel_2.png",
+    ],
+    "ORC_PANEL": [
+        "graphics/tilesets/forest/portrait_icons.png",
+        "graphics/ui/gold_icon_1.png",
+        "graphics/ui/lumber_icon_1.png",
+        "graphics/ui/percent_complete.png",
+        "graphics/ui/orc/icon_border.png",
+        "graphics/ui/orc/icon_selection_boxes.png",
+        "graphics/ui/orc/panel_2.png",
+    ],
+    "FOREST_TILESET": [
+        "graphics/tilesets/forest/terrain.png",
+        "graphics/human/units/*.png",
+        "graphics/neutral/units/*.png",
+        "graphics/orc/units/*.png",
+        "graphics/missiles/*.png",
+        "graphics/tilesets/forest/human/buildings/*.png",
+        "graphics/tilesets/forest/orc/buildings/*.png",
+        "graphics/tilesets/forest/neutral/buildings/*.png",
+    ],
+    "SWAMP_TILESET": [
+        "graphics/tilesets/swamp/terrain.png",
+        "graphics/human/units/*.png",
+        "graphics/neutral/units/*.png",
+        "graphics/orc/units/*.png",
+        "graphics/missiles/*.png",
+        "graphics/tilesets/swamp/human/buildings/*.png",
+        "graphics/tilesets/swamp/orc/buildings/*.png",
+        "graphics/tilesets/swamp/neutral/buildings/*.png",
+    ],
+    "DUNGEON_TILESET": [
+        "graphics/tilesets/dungeon/terrain.png",
+        "graphics/human/units/*.png",
+        "graphics/neutral/units/*.png",
+        "graphics/orc/units/*.png",
+        "graphics/missiles/*.png",
+        "graphics/tilesets/dungeon/neutral/buildings/*.png",
+    ],
+}
+
+def system(s):
+    code = os.system(s)
+    if code != 0:
+        import pdb; pdb.set_trace()
+    return
+
+def main(data, out, bindir):
+    rgb2amiga = os.path.join(bindir, 'bin', 'Rgb2Amiga')
+    bitmap_conv = os.path.join(bindir, 'bin', 'bitmap_conv')
+    tileset_conv = os.path.join(bindir, 'bin', 'tileset_conv')
+    palette_conv = os.path.join(bindir, 'bin', 'palette_conv')
+    w_h_re = re.compile(r"PNG image data, (\d+) x (\d+),")
+    transparency = r'\#BBBBBB'
+
+    data = os.path.join(data, ".")
+    out = os.path.join(out, ".")
+
+    for name,set in DATA.items():
+        inputfiles = []
+        pngfiles = []
+        bmfiles = []
+        for pattern in set:
+            for i in glob.glob(os.path.join(data, pattern)):
+                output = i.replace(data, os.path.join(out, name.lower()))
+                inputfiles.append(i)
+                pngfiles.append(output)
+                os.makedirs(os.path.dirname(output), exist_ok=True)
+                bmfiles.append(output.replace(".png", ".bm"))
+
+        inargs = " -i ".join(inputfiles)
+        outargs = " -o ".join(pngfiles)
+        system(f"{rgb2amiga} -c 32 -f png-gpl -s ! -i {inargs} -o {outargs}")
+        palette = os.path.join(out, f"{name.lower()}.plt")
+        system(f"{palette_conv} {pngfiles[0]}.gpl {palette}")
+        for bmfile,pngfile in zip(bmfiles, pngfiles):
+            # TODO: mask color is transparency
+            info = subprocess.check_output(f"file {pngfile}.png", shell=True).decode()
+            w, h = (int(x) for x in w_h_re.search(info).groups())
+            if w % 16:
+                if w > 16:
+                    crop_left = math.floor((w % 16) / 2)
+                    crop_right = math.ceil((w % 16) / 2)
+                    system(f"convert {pngfile}.png -gravity East -crop {w - crop_left}x{h}+0+0 +repage {pngfile}.png")
+                    system(f"convert {pngfile}.png -gravity West -crop {w - crop_left - crop_right}x{h}+0+0 +repage {pngfile}.png")
+                else:
+                    add_left = math.floor((16 - w) / 2)
+                    add_right = math.ceil((16 - w) / 2)
+                    system(f"convert {pngfile}.png -gravity East -background {transparency} -splice {add_left}x0 +repage {pngfile}.png")
+                    system(f"convert {pngfile}.png -gravity West -background {transparency} -splice {add_right}x0 +repage {pngfile}.png")
+            system(f"{bitmap_conv} {palette} {pngfile}.png -o {bmfile} -mc {transparency}")
+        if name.endswith("_TILESET"):
+            system(f"{tileset_conv} {pngfiles[0]}.png {TILE_SIZE} {bmfiles[0]} -plt {palette}")
+
+
+if __name__ == "__main__":
+    import sys
+    from argparse import ArgumentParser
+    parser = ArgumentParser(sys.argv[0])
+    parser.add_argument('--data', required=True)
+    parser.add_argument('--output', required=True)
+    parser.add_argument('--prefix', required=True)
+    parsed_args = parser.parse_args(sys.argv[1:])
+    main(parsed_args.data, parsed_args.output, parsed_args.prefix)
--- src/game.c
+++ src/game.c
@@ -1,31 +1,200 @@
 #include "game.h"
-#include <ace/managers/key.h> // We'll use key* fns here
-#include <ace/managers/game.h> // For using gameExit
-#include <ace/managers/system.h> // For systemUnuse and systemUse
+#include "bob_new.h"
+#include <ace/managers/copper.h>
+#include <ace/managers/log.h>
+#include <ace/managers/mouse.h>
+#include <ace/types.h>
+#include <ace/managers/viewport/camera.h>
+#include <ace/utils/bitmap.h>
+#include <ace/utils/custom.h>
+#include <ace/utils/extview.h>
+#include <ace/utils/file.h>
+#include <ace/utils/palette.h>
+#include <ace/managers/key.h>
+#include <ace/managers/game.h>
+#include <ace/managers/system.h>
+#include <ace/managers/viewport/simplebuffer.h>
+#include <ace/managers/viewport/scrollbuffer.h>
+#include <ace/managers/viewport/tilebuffer.h>
+#include <ace/managers/blit.h>
+#include <stdint.h>
 
-// "Gamestate" is a long word, so let's use shortcut "Gs" when naming fns
+
+#define TILE_SHIFT 4
+#define TILE_SIZE 1 << TILE_SHIFT
+#define MAP_SIZE 64
+
+#define BPP 5
+#define COLORS 1 << BPP
+
+static tView *s_pView; // View containing all the viewports
+static tVPort *s_pVpPanel; // Viewport for panel
+static tSimpleBufferManager *s_pPanelBuffer;
+static tVPort *s_pVpMain; // Viewport for playfield
+static tTileBufferManager *s_pMapBuffer;
+static tCameraManager *s_pMainCamera;
+static tBitMap *s_pMapBitmap;
+
+static tBitMap *s_pGoldMineBitmap;
+static tBitMap *s_pGoldMineMask;
+static tBobNew s_GoldMineBob;
+
+// palette switching
+static uint16_t s_pMapPalette[COLORS];
+static uint16_t s_pPanelPalette[COLORS];
+static tCopBlock *s_pPaletteBlocks[2];
+
+#define MAP_HEIGHT 200
+#define PANEL_HEIGHT 48
+
+/*
+ * void myTileDrawCallback(UWORD uwTileX, UWORD uwTileY,
+ *                         tBitMap *pBitMap, UWORD uwBitMapX, UWORD uwBitMapY) {
+ *     _logWrite("Drawing tile %d:%d\n", uwTileX, uwTileY);
+ * }
+ */
 
 void gameGsCreate(void) {
-  // Initializations for this gamestate - load bitmaps, draw background, etc.
-  // We don't need anything here right now except for unusing OS
-  systemUnuse();
+    logWrite("load view 0\n");
+    viewLoad(0);
+    logWrite("system use\n");
+
+    logWrite("Create view\n");
+    s_pView = viewCreate(0,
+                         TAG_VIEW_GLOBAL_CLUT, 1,
+                         TAG_VIEW_COPLIST_MODE, VIEW_COPLIST_MODE_BLOCK,
+                         TAG_DONE);
+
+    logWrite("Create map\n");
+
+    s_pGoldMineBitmap = bitmapCreateFromFile("resources/forest_tileset/graphics/tilesets/forest/neutral/buildings/gold_mine.bm", 0);
+    s_pGoldMineMask = bitmapCreateFromFile("resources/forest_tileset/graphics/tilesets/forest/neutral/buildings/gold_mine_mask.bm", 0);
+
+    // create map area
+    paletteLoad("resources/forest_tileset.plt", s_pMapPalette, COLORS);
+    s_pPaletteBlocks[0] = copBlockCreate(s_pView->pCopList, COLORS, 0, 0);
+    for (uint8_t i = 0; i < COLORS; i++) {
+        copMove(s_pView->pCopList, s_pPaletteBlocks[0], &g_pCustom->color[i], s_pMapPalette[i]);
+    }
+
+    s_pVpMain = vPortCreate(0,
+                            TAG_VPORT_VIEW, s_pView,
+                            TAG_VPORT_BPP, BPP,
+                            TAG_VPORT_HEIGHT, MAP_HEIGHT,
+                            TAG_END);
+    for (uint8_t i = 0; i < COLORS; i++) {
+        s_pVpMain->pPalette[i] = s_pMapPalette[i];
+    }
+    s_pMapBitmap = bitmapCreateFromFile("resources/forest_tileset/graphics/tilesets/forest/terrain.bm", 0);
+    logWrite("Create tilebuffer\n");
+    s_pMapBuffer = tileBufferCreate(0,
+                                    TAG_TILEBUFFER_VPORT, s_pVpMain,
+                                    TAG_TILEBUFFER_BITMAP_FLAGS, BMF_CLEAR,
+                                    TAG_TILEBUFFER_BOUND_TILE_X, MAP_SIZE,
+                                    TAG_TILEBUFFER_BOUND_TILE_Y, MAP_SIZE,
+                                    TAG_TILEBUFFER_TILE_SHIFT, TILE_SHIFT,
+                                    TAG_TILEBUFFER_TILESET, s_pMapBitmap,
+                                    TAG_TILEBUFFER_IS_DBLBUF, 0,
+                                    TAG_TILEBUFFER_REDRAW_QUEUE_LENGTH, 9,
+                                    TAG_END);
+    s_pMainCamera = s_pMapBuffer->pCamera;
+    logWrite("set camera coords\n");
+    cameraSetCoord(s_pMainCamera, 0, 0);
+
+    bobNewManagerCreate(s_pMapBuffer->pScroll->pFront, s_pMapBuffer->pScroll->pBack, s_pMapBuffer->pScroll->uwBmAvailHeight);
+    bobNewInit(&s_GoldMineBob, 64, 64, 0, s_pGoldMineBitmap, s_pGoldMineMask, 80, 80);
+    bobNewReallocateBgBuffers();
+
+    logWrite("file tile data\n");
+    tFile *map = fileOpen("resources/maps/orc12.map", "r");
+    for (int x = 0; x < MAP_SIZE; x++) {
+        fileRead(map, s_pMapBuffer->pTileData[x], MAP_SIZE);
+    }
+    fileClose(map);
+
+    logWrite("redraw all\n");
+    tileBufferRedrawAll(s_pMapBuffer);
+
+    // create panel area
+    paletteLoad("resources/human_panel.plt", s_pPanelPalette, COLORS);
+    s_pPaletteBlocks[1] = copBlockCreate(s_pView->pCopList, COLORS, 0, MAP_HEIGHT + 45);
+    for (uint8_t i = 0; i < COLORS; i++) {
+        copMove(s_pView->pCopList, s_pPaletteBlocks[1], &g_pCustom->color[i], s_pPanelPalette[i]);
+    }
+    
+    s_pVpPanel = vPortCreate(0,
+                             TAG_VPORT_VIEW, s_pView,
+                             TAG_VPORT_BPP, BPP,
+                             // TAG_VPORT_OFFSET_TOP, 1,
+                             TAG_VPORT_HEIGHT, PANEL_HEIGHT,
+                             TAG_END);
+    logWrite("create panel buffer\n");
+    s_pPanelBuffer = simpleBufferCreate(0,
+                                        TAG_SIMPLEBUFFER_VPORT, s_pVpPanel,
+                                        TAG_SIMPLEBUFFER_BITMAP_FLAGS, BMF_CLEAR,
+                                        TAG_END);
+    bitmapLoadFromFile(s_pPanelBuffer->pFront, "resources/human_panel/graphics/ui/human/panel_2.bm", 48, 0);
+
+    // finish up
+    logWrite("load view\n");
+    viewLoad(s_pView);
+    logWrite("unuse system\n");
+    systemUnuse();
 }
 
+static uint16_t GameCycle = 0;
+
 void gameGsLoop(void) {
-  // This will loop forever until you "pop" or change gamestate
-  // or close the game
-  if(keyCheck(KEY_ESCAPE)) {
-    gameExit();
-  }
-  else {
-    // Process loop normally
-    // We'll come back here later
-  }
+    switch (GameCycle % 1) {
+    case 0:
+        // This will loop every frame
+        if (keyCheck(KEY_W)) {
+            cameraMoveBy(s_pMainCamera, 0, -1);
+        }
+        if (keyCheck(KEY_S)) {
+            cameraMoveBy(s_pMainCamera, 0, 1);
+        }
+        if (keyCheck(KEY_A)) {
+            cameraMoveBy(s_pMainCamera, -1, 0);
+        }
+        if (keyCheck(KEY_D)) {
+            cameraMoveBy(s_pMainCamera, 1, 0);
+        }
+        if (keyCheck(KEY_ESCAPE)) {
+            gameExit();
+        }
+        if (keyCheck(KEY_C)) {
+            copDumpBlocks();
+        }
+        if (mouseGetX(MOUSE_PORT_1) > s_pVpMain->uwWidth - 5) {
+            cameraMoveBy(s_pMainCamera, 1, 0);
+        } else if (mouseGetX(MOUSE_PORT_1) < 5) {
+            cameraMoveBy(s_pMainCamera, -1, 0);
+        } else if (mouseGetY(MOUSE_PORT_1) < 5) {
+            cameraMoveBy(s_pMainCamera, 0, -1);
+        } else if (mouseGetY(MOUSE_PORT_1) > s_pVpMain->uwHeight - 5) {
+            cameraMoveBy(s_pMainCamera, 0, 1);
+        }
+    }
+
+    bobNewBegin(s_pMapBuffer->pScroll->pBack);
+    if (tileBufferIsTileOnBuffer(s_pMapBuffer, 80 / 16, 80 / 16)) {
+        bobNewPush(&s_GoldMineBob);
+    }
+    bobNewPushingDone();
+    bobNewProcessNext();
+    bobNewEnd();
+
+    viewProcessManagers(s_pView);
+    copProcessBlocks();
+    vPortWaitForEnd(s_pVpPanel);
+    GameCycle++;
 }
 
 void gameGsDestroy(void) {
-  systemUse();
-  // Cleanup when leaving this gamestate
-  // Empty at the moment except systemUse
-}
+    systemUse();
 
+    // This will also destroy all associated viewports and viewport managers
+    bobNewManagerDestroy();
+    viewDestroy(s_pView);
+}
